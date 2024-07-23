fc35eca11ce0fff9052c69e397eaae0f22b2ddd5
Luke Parkin <luke.parkin@arm.com>
2024-07-23 16:48:49 +0100

    Initial MVP of a ticket board
    Create an initial django project
    With one view for a very basic ticket adding and viewing system
    Add the initial model for a ticket (for django db)
    
    Signed-off-by: Luke Parkin <luke.parkin@arm.com>
    v1->v2: Include manage.py
    v2->v3: Add requirements.txt

---
+++ tickets/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .models import Ticket
+
+
+def ticket_list(request):
+    # If posting to this endpoint, a ticket is being sent.
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        Ticket.objects.create(title=title, description=description)
+        return redirect("ticket_list")
+
+    tickets = Ticket.objects.all().order_by("-created_at")
+    return render(request, "board.html", {"tickets": tickets})
