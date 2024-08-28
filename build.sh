8a1a7b21b9551cf49e32d848f4be3f02eb831fa7
Luke Parkin <luke.parkin@arm.com>
2024-08-28 13:53:31 +0100

    Makemigrations before migrating on prod build

--- build.sh
+++ build.sh
@@ -8,4 +8,6 @@ python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --verbosity 2
 
 # Apply any outstanding database migrations
+python3 manage.py makemigrations
+
 python3 manage.py migrate
